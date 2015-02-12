6eb75f2c786647b59fd1b76d86dc8f9b13f37e1a
Peter Orum <peter@peterorum.com>
2015-02-12 23:15:02 +1100

    add pickRandom ley

--- plus-fp.js
+++ plus-fp.js
@@ -3,7 +3,9 @@
     "use strict";
 
     var fp = require('lodash-fp');
+    var math = require('mathjs');
 
+    // returns true if any predicates is true
     exports.anyOf = fp.curry(function(arr, w)
     {
         return fp.any(function(f)
@@ -12,6 +14,7 @@
         }, arr);
     });
 
+    // returns true if all predicates are true
     exports.allOf = fp.curry(function(arr, w)
     {
         return fp.all(function(f)
@@ -19,4 +22,22 @@
             return f(w);
         }, arr);
     });
+
+    // pick a random property
+    exports.pickRandom = fp.curry(function(x)
+    {
+        var key = math.pickRandom(fp.keys(x));
+        var val = x[key];
+
+        // return property name in object
+
+        if (/object|function/.test(val))
+        {
+            val.key = key;
+        }
+
+        return val;
+
+    });
+
 })();
