4af13862c3875652e38bb3ea63c699ac3968ccfb
Peter Orum <peter@peterorum.com>
2015-02-14 13:23:06 +1100

    wandom

--- plus-fp.js
+++ plus-fp.js
@@ -36,4 +36,49 @@
         return x[exports.pickRandomKey(x)];
     });
 
+    // pick a weighted random
+    exports.wandom = fp.curry(function(vec)
+    {
+        // if array objects have a weight property, use it
+
+
+        var pick;
+
+        if (vec.length && fp.has('weight', vec[0]))
+        {
+            var weights = fp.pluck('weight', vec);
+
+            // sum the weights to normalize them
+            var sum = fp.reduce(function(sum, n)
+            {
+                return sum + n;
+            }, 0, weights);
+
+            if (sum)
+            {
+
+                var probs = fp.map(function(w)
+                {
+                    return w / sum;
+                }, weights);
+
+        debugger;
+
+                var r = math.random(0, 1);
+
+                var rsum = 0;
+                var i = -1;
+
+                do {
+                    i += 1;
+                    rsum += probs[i];
+                } while (rsum <= r && i < vec.length - 1);
+
+                pick = vec[i];
+            }
+
+            return pick;
+        }
+    });
+
 })();
